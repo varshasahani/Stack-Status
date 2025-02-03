c5f1788283ad74d2a141d8b8b6e0ae93c7e5cca9
Varsha Sahani <varsha.sahani@nvent.com>
2025-02-03 19:19:48 +0530

    updated packages for deployment

--- src/backend/server.js
+++ src/backend/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./dist/stackStatus'));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
